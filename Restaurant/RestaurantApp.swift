47a677079a988f9acda21b8f496423b43a782cd7
PiotrK <piotr.koscielny69@gmail.com>
2025-06-02 14:57:01 +0200

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by piotr koscielny on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
